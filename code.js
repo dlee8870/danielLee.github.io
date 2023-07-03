4338bbe2fda02f824252def1e404a8ca81e13418
dlee8870 <125638043+dlee8870@users.noreply.github.com>
2023-07-03 20:49:03 +0000

    Update

---
+++ code.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".gen-type", {
+    strings: ["I'm a student", "I'm a gamer", "I'm a musician", "I'm a homebody"],
+    typeSpeed: 200,
+    backSpeed: 200,
+    loop: true
+});
