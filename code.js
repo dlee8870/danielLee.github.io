19c4cab52ef568f3394929ae0077b0572323f873
dlee8870 <125638043+dlee8870@users.noreply.github.com>
2023-08-15 01:42:34 +0000

    Changes to js

--- code.js
+++ code.js
@@ -1,7 +1,17 @@
-
 var typed = new Typed(".gen-type", {
     strings: ["I'm a student", "I'm a gamer", "I'm a musician", "I'm a homebody"],
     typeSpeed: 200,
     backSpeed: 200,
     loop: true
 });
+
+function MyFunction() {
+    let element = document.body;
+    element.classList.toggle("dark")
+}
+
+
+$(document).ready(function() {
+    // Add the 'hovered' class to the Home nav-link when the page loads
+    $("a[href='#home']").addClass("hovered");
+  });
